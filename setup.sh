8f6773216528801569591f8f5cfe221c57e9ecaf
Malik Zurkiyeh <malik.zurkiyeh@gmail.com>
2025-08-28 12:37:07 -0400

    Reverted to previous commit

--- setup.sh
+++ setup.sh
@@ -5,5 +5,19 @@ if (( $EUID != 0)); then
     exit
 fi
 
-apt-get update && apt-get upgrade
-apt-get install -y --no-install-recommends gcc curl openssl mariadb-server libmariadb3 libmariadb-dev python3 python3-pip ipython3 python3-dev python3-venv gunicorn pkg-config
+#apt-get update && apt-get upgrade
+#apt-get install -y --no-install-recommends gcc curl openssl mariadb-server libmariadb3 libmariadb-dev python3 python3-pip ipython3 python3-dev python3-venv gunicorn pkg-config
+
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    gcc \
+    libmariadb3 \
+    libmariadb-dev \
+    python3 \
+    python3-pip \
+    ipython3 \
+    python3-dev \
+    python3-venv \
+    gunicorn \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
